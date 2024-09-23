9ffe5bfc3ada193ed8053d2754a4c236c892a812
Mariam Kmayha <Mariam.Kmayha@tecalliance.net>
2024-09-23 14:32:18 +0200

    test

---
+++ mosbach-tasks-rest-main/src/main/Datenbank/Student.java
@@ -0,0 +1,9 @@
+
+import java.util.List;
+
+public class Student {
+    public List<Vorlesung> vorlesung;
+    
+
+
+}
---
+++ mosbach-tasks-rest-main/src/main/Datenbank/Vorlesung.java
@@ -0,0 +1,9 @@
+
+import java.util.List;
+
+public class Vorlesung {
+    public List<Student> teilnehmer;
+    
+
+
+}
